cf05c983a6823351b81e880b1b9c71b2f3f14868
Abhinav Anand <abhinav_anand@live.com>
2018-11-03 23:48:09 +0530

    Wire up sqlalchemy and models.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,11 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+db = SQLAlchemy(app)
+# TODO: setup db schema and data dump https://stackoverflow.com/a/846665/5463404
 
 from app import views
+
+from app import models
