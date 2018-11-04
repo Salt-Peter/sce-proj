2da524707c1ccf595d3fc0ac412b3473813c8b6c
Abhinav Anand <abhinav_anand@live.com>
2018-11-04 13:17:32 +0530

    Add Subscription and Like models

--- iiit_research/models.py
+++ iiit_research/models.py
@@ -28,6 +28,27 @@ class Post(db.Model):
     date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
     content = db.Column(db.Text, nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    likes = db.relationship('Like', backref='post', lazy=True)
 
     def __repr__(self):
         return f"Post('{self.title}','{self.date_posted}')"
+
+
+class Subscription(db.Model):
+    # follower = subscriber
+    follower = db.Column(db.Integer, db.ForeignKey('user.id'), primary_key=True, nullable=False)
+    # followee = publisher
+    followee = db.Column(db.Integer, db.ForeignKey('user.id'), primary_key=True, nullable=False)
+
+    from sqlalchemy import CheckConstraint
+
+    # FIXME: Perhaps this constraint should be handled in application logic
+    __table_args__ = (
+        # Make sure a user can not follow himself.
+        CheckConstraint(follower != followee, name='check_follower_not_followee'),
+    )
+
+
+class Like(db.Model):
+    post_id = db.Column(db.Integer, db.ForeignKey('post.id'), primary_key=True, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), primary_key=True, nullable=False)
