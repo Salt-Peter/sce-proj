dca5d0faff8d670787cc18bc1f9e002da2e4cc69
Abhinav Anand <abhinav_anand@live.com>
2018-11-04 07:49:15 +0530

    Format files as per PEP8
    https://www.python.org/dev/peps/pep-0008/

--- iiit_research/routes.py
+++ iiit_research/routes.py
@@ -1,25 +1,29 @@
 import os
 import secrets
-from flask import render_template,url_for, flash, redirect, request
-from iiit_research import app,db,bcrypt
+
+from flask import render_template, url_for, flash, redirect, request
+from flask_login import login_user, current_user, logout_user, login_required
+
+from iiit_research import app, db, bcrypt
 from iiit_research.forms import RegistrationForm, LoginForm, UpdateAccountForm, PostForm
 from iiit_research.models import User, Post
-from flask_login import login_user, current_user, logout_user, login_required
 
 
 @app.route("/")
 @app.route("/home")
 def home():
-    return render_template('home.html',title='Home')
+    return render_template('home.html', title='Home')
+
 
 @app.route("/about")
 def about():
-    return render_template('about.html',title='About')
+    return render_template('about.html', title='About')
+
 
 @app.route("/posts")
 def post():
     posts = Post.query.all()
-    return render_template('posts.html',posts=posts,title='Posts')
+    return render_template('posts.html', posts=posts, title='Posts')
 
 
 @app.route("/register", methods=['GET', 'POST'])
@@ -29,22 +33,23 @@ def register():
     form = RegistrationForm()
     if form.validate_on_submit():
         hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
-        user = User(username = form.username.data, email = form.email.data, password = hashed_password)
+        user = User(username=form.username.data, email=form.email.data, password=hashed_password)
         db.session.add(user)
         db.session.commit()
         flash(f'Your account is created! You can login Now.', 'success')
         return redirect(url_for('login'))
     # else:
     #     flash(f'Wrong information!', 'danger')
-    return render_template('register.html', title='Register', form = form)
+    return render_template('register.html', title='Register', form=form)
 
-@app.route("/login", methods=['GET','POST'])
+
+@app.route("/login", methods=['GET', 'POST'])
 def login():
     if current_user.is_authenticated:
         return redirect(url_for('home'))
     form = LoginForm()
     if form.validate_on_submit():
-        user = User.query.filter_by(email = form.email.data).first()
+        user = User.query.filter_by(email=form.email.data).first()
         if user and bcrypt.check_password_hash(user.password, form.password.data):
             login_user(user, remember=form.remember.data)
             next_page = request.args.get('next')
@@ -53,22 +58,25 @@ def login():
             flash('Login Unsuccessful. Please check username and password', 'danger')
         # flash(f'Successfully Logged in!', 'success')
         # return redirect(url_for('home'))
-    return render_template('login.html', title='Login', form = form)
+    return render_template('login.html', title='Login', form=form)
+
 
 @app.route("/logout")
 def logout():
     logout_user()
     return redirect(url_for('login'))
 
+
 def save_pic(form_pic):
     random_hex = secrets.token_hex(8)
     fname, fext = os.path.splitext(form_pic.filename)
-    pic_fn = random_hex+fext
+    pic_fn = random_hex + fext
     pic_path = os.path.join(app.root_path, 'static/profile_pics/', pic_fn)
     form_pic.save(pic_path)
     return pic_fn
 
-@app.route("/account", methods=['GET','POST'])
+
+@app.route("/account", methods=['GET', 'POST'])
 @login_required
 def account():
     form = UpdateAccountForm()
@@ -84,17 +92,18 @@ def account():
     elif request.method == 'GET':
         form.username.data = current_user.username
         form.email.data = current_user.email
-    image_file = url_for('static', filename='profile_pics/'+current_user.image_file)
-    return render_template('account.html', title='Account', image_file = image_file, form = form)
+    image_file = url_for('static', filename='profile_pics/' + current_user.image_file)
+    return render_template('account.html', title='Account', image_file=image_file, form=form)
 
-@app.route("/post/new", methods=['GET','POST'])
+
+@app.route("/post/new", methods=['GET', 'POST'])
 @login_required
 def new_post():
     form = PostForm()
     if form.validate_on_submit():
-        post = Post(title = form.title.data, content = form.content.data, author = current_user)
+        post = Post(title=form.title.data, content=form.content.data, author=current_user)
         db.session.add(post)
         db.session.commit()
         flash('Your post has been created!', 'success')
         return redirect(url_for('home'))
-    return render_template('create_post.html', title='New Post', form = form) 
+    return render_template('create_post.html', title='New Post', form=form)
