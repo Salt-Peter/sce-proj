927098273b0dd3d9c94b8b2d445decd747950705
Abhinav Anand <abhinav_anand@live.com>
2018-11-08 12:35:02 +0530

    Add db dumps

--- sql_dumps/main_subscription.sql
+++ sql_dumps/main_subscription.sql
@@ -1,2 +1,4 @@
-INSERT INTO subscription (follower, followee) VALUES (2, 1);
-INSERT INTO subscription (follower, followee) VALUES (3, 1);
+INSERT INTO subscription (id, follower, followee) VALUES (1, 2, 1);
+INSERT INTO subscription (id, follower, followee) VALUES (2, 3, 1);
+INSERT INTO subscription (id, follower, followee) VALUES (3, 1, 2);
+INSERT INTO subscription (id, follower, followee) VALUES (4, 1, 3);
