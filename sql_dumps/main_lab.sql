7c766d1b32b375118d9f5e329477f793b4bbd6b6
Abhinav Anand <abhinav_anand@live.com>
2018-11-11 19:51:29 +0530

    Add some data to labs table

---
+++ sql_dumps/main_lab.sql
@@ -0,0 +1,16 @@
+INSERT INTO lab (id, name, description, image) VALUES (1, 'Software Engineering Research Center (SERC)', 'Software engineering is the engineering of large and complex software systems. It focuses on systematic, disciplined, quantifiable approach to the development, operation, and maintenance of quality software, and the study of these approaches. SERC aims to lead the software engineering research in India by addressing India-specific issues in an industry-driven environment.
+
+The main objectives of SERC are to focus on software engineering research, research in software engineering issues in the context of Indian software industry, operate closely with the industry by establishing touch points and to be a center of excellence driven by the industry for the industry.
+
+Some of the major funded projects are state variable approach to the model-driven development of software for reactive systems (funded by SIEMENS), virtual Labs Integration (funded by MHRD), standardization of mobile interfaces (funded by CA Technologies) and big data analytics on the cloud (funded by CA Technologies).', 'serc.jpg');
+INSERT INTO lab (id, name, description, image) VALUES (2, 'Search and Information Extraction Lab (SIEL)', 'The Lab focuses on solving research problems in the areas of Information Retrieval and Extraction using NLP techniques. 
+SIEL is engaged in building technologies for personalized, customizable and highly relevant information retrieval (IR), Information Extraction (IE) and Information Access (IA) systems. Current research includes summarization, cross language information access systems and building semantic search engines by identifying entities and relations between these entities.
+
+', null);
+INSERT INTO lab (id, name, description, image) VALUES (3, 'Center for Visual Information Technology (CVIT)', 'CVIT focuses on basic and advanced research in image processing, computer vision, computer graphics and machine learning. This center deals with the generation, processing, and understanding of primarily visual data as well as with the techniques and tools required doing so efficiently. The activity of this center overlaps the traditional areas of Computer Vision, Image Processing, Computer Graphics, Pattern Recognition and Machine Learning. CVIT works on both theoretical as well as practical aspects of visual information processing. Center aims to keep the right balance between the cutting edge academic research and impactful applied research.
+
+Research projects are funded by various agencies like- Department of Science and Technology (DST), Naval Research Board (NRB), Ministry of Communications and Information Technology (MCIT), Defence Research and Development Organisation (DRDO), General Electric (GE), Nvidia and many more.
+
+CVIT has around 50 students carrying out research for M.S. and Ph.D. degrees.
+
+The center has regular visits and talks by leading researchers in the world.', 'cvit.jpg');
