b1d9aa7d511e636623a8d7964cabcdabf241b02e
Abhinav Anand <abhinav_anand@live.com>
2018-11-12 00:13:29 +0530

    Add dummy data to db and update sqldumps

--- sql_dumps/main_subscription.sql
+++ sql_dumps/main_subscription.sql
@@ -1,4 +1,5 @@
-INSERT INTO subscription (id, follower, followee) VALUES (1, 2, 1);
-INSERT INTO subscription (id, follower, followee) VALUES (2, 3, 1);
-INSERT INTO subscription (id, follower, followee) VALUES (3, 1, 2);
-INSERT INTO subscription (id, follower, followee) VALUES (4, 1, 3);
+INSERT INTO subscription (id, follower, followee, followee_type) VALUES (1, 2, 1, 'user');
+INSERT INTO subscription (id, follower, followee, followee_type) VALUES (2, 3, 1, 'user');
+INSERT INTO subscription (id, follower, followee, followee_type) VALUES (3, 1, 2, 'user');
+INSERT INTO subscription (id, follower, followee, followee_type) VALUES (4, 1, 3, 'user');
+INSERT INTO subscription (id, follower, followee, followee_type) VALUES (5, 2, 1, 'lab');
